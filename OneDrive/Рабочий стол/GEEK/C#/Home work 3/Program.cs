c543abf9343a2815e8b77f829387bb326401c8e3
OLEG <olegruz85@gmail.com>
2022-08-15 20:25:32 +0200

    Готово ДЗ

---
+++ OneDrive/Рабочий стол/GEEK/C#/Home work 3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+//  Console.Write("Введите Координаты точки xa: ");
+//  int xa = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите Координаты точки xb: ");
+// int xb = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите Координаты точки ya: ");
+// int ya = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Введите Координаты точки yb: ");
+// int yb = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите Координаты точки za: ");
+// int za = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Введите Координаты точки zb: ");
+// int zb = Convert.ToInt32(Console.ReadLine());
+
+
+
+//  double result = 0; // тип данных где есть 2 знака после запятой
+
+// result = Math.Sqrt(Math.Pow(xb - xa,2) + Math.Pow(yb - ya,2) + Math.Pow(zb - za,2) );
+// Console.WriteLine($"Result: {Math.Round (result,2)}"); // $ интерполяция
+
+
+
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int start = 1;
+// while (start <=N)
+// {
+//   Console.WriteLine(Math.Pow (start,3));
+// start++;
+//  }
+
+
+// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// Console.Write("Введите пятизначное число : ");
+// string Polindrom = Console.ReadLine ();
+// if ((Polindrom [0]==Polindrom[4]) && (Polindrom [1]==Polindrom[3]))
+// {
+//     Console.Write ("-> да");
+// }
+// else
+// {
+//    Console.Write("-> нет");
+// }
+
